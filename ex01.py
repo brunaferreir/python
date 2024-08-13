e6eb8ee7e7589fb23e15de0f91b8b0aeb543c35e
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2024-08-13 18:45:46 -0300

    Add files via upload

---
+++ ex01.py
@@ -0,0 +1,7 @@
+#Exercício 1: Números Primos
+#Crie um programa que encontre e imprima os 20 primeiros números primos.
+#Um número é considerado primo se ele é maior que 1 e possui exatamente
+#dois divisores: 1 e ele mesmo. Utilize um loop while para iterar até
+#encontrar 20 números primos e um loop for para contar a quantidade de
+#divisores de cada número.'''
+
---
+++ ex03.py
@@ -0,0 +1,6 @@
+#Exercício 3: Remoção de Números Pares
+#Implemente uma função chamada remover_pares que receba uma lista de
+#números inteiros e retorne uma nova lista contendo apenas os números
+#ímpares. Utilize um loop for para percorrer a lista e verifique se cada
+#número é ímpar. Por fim, imprima a lista original e a lista resultante
+#com os números ímpares.'''
