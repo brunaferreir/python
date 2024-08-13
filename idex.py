8712d8f90d541e0ee7a1bcbf67845b3b7c1d2ae5
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2024-08-13 18:29:20 -0300

    Add files via upload

---
+++ idex.py
@@ -0,0 +1,15 @@
+# 2. Determinar se um ano fornecido pelo usuário é bissexto ou não
+# Lembre-se que um ano é bissexto se for divisível por 4 e, se for divisível por
+#100, deve ser também divisível por 400.
+
+ano= int(input("Digite o ano"))
+
+if ano %4 == 0 or ano %100 == 0 and ano %400 == 0:
+    print("E bi ")
+else:
+    print("nao e bi")
+
+
+
+
+
