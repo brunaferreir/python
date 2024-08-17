f191bd75318498df3969f70dc214fe8e55073d6a
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2024-08-16 23:32:06 -0300

    organizando

--- ex04.py
+++ ex04.py
@@ -1,4 +1,4 @@
-# 2. Determinar se um ano fornecido pelo usuário é bissexto ou não
+# 4. Determinar se um ano fornecido pelo usuário é bissexto ou não
 # Lembre-se que um ano é bissexto se for divisível por 4 e, se for divisível por
 #100, deve ser também divisível por 400.
 
