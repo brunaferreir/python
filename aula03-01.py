6fce28d80a91fa4b786600baee72dce401df2bbb
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2024-08-26 14:57:58 -0300

    atualizando

--- aula03-01.py
+++ aula03-01.py
@@ -7,3 +7,15 @@ superior a R$ 50,00.
 Exemplo:
 Veja um exemplo da estrutura do dicionário.
 {&#39;caneta&#39;: 3.0, &#39;Pen drive&#39;: 100.0,&#39;Teclado&#39;: 30.0, &#39;Lápis&#39;: 1.5}'''
+
+produtos = {}
+
+while len(produtos) < 5:
+    chave = input('Insira o produto:' )
+    valor = float(input('Insira o valor do produto:' ))
+    produtos[chave] = valor
+
+for chave, valor in produtos.items():
+    if valor > 50:
+        print(chave, valor)
+
--- aula03-02.py
+++ aula03-02.py
@@ -6,3 +6,19 @@ Preencha um dicionário com os dados de 5 alunos
 Exemplo:
 Veja um exemplo da estrutura do dicionário.
 {19230490: [10, 8.0, 7.5], 2002441: [6, 5, 7.5], 2001332: [5,8,9.5]}'''
+
+
+dados = {}
+
+while len(dados) < 5:
+    notas = []
+    ra = float(input("Insira o RA: "))
+    for i in range(3):
+        nota = float(input("Insira a nota: "))
+        notas.append(nota)
+        dados[ra] = notas
+
+for chave, valor in dados.items():
+    print(f'RA: {chave}')
+    print(f'Notas: {valor}')
+    print(f'Média: {(sum(valor))/len(valor):.2f}')
