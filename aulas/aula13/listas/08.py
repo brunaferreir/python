20b41cb072cc218a29b21d37beb1078ef8d4c19f
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2025-02-08 19:34:28 -0300

    lista

---
+++ aulas/aula13/listas/08.py
@@ -0,0 +1,8 @@
+#crie um programa onde o usuario possa digitar cinco valores numericos e cadastre-osem uma lista.ja na posição  correta de inserção (sem usar o sort())
+# No final mostre a lista ordenada na tela. 
+
+lista = []
+for cont in range(1,6):
+    lista.append(int(input('Digite um valor ')))
+
+print(lista)    
---
+++ aulas/aula13/listas/10.py
@@ -0,0 +1,5 @@
+#Crie um progrma que vai ler varios numeros e colocar em uma lista.
+#depois mostre:
+# A)Quantos numeros foram digitados
+#B)A lista ordenada de forma decrescente.
+#C)Se po valor 5 foi digitado e esta ou nao na lista.
