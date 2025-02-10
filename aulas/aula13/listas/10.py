52220ca38c2a8e15d89a1984e4105ebf9d547da2
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2025-02-09 21:38:09 -0300

    lista

--- aulas/aula13/listas/10.py
+++ aulas/aula13/listas/10.py
@@ -2,4 +2,28 @@
 #depois mostre:
 # A)Quantos numeros foram digitados
 #B)A lista ordenada de forma decrescente.
-#C)Se po valor 5 foi digitado e esta ou nao na lista.
+#C)Se o valor 5 foi digitado e esta ou nao na lista.
+
+def adicionar(valores, elemento): 
+     valores.append(elemento)      
+valores = []
+
+r = 'S'
+while r == 'S':
+    try:
+        elemento = int(input(f'Digite um numero '))
+        adicionar(valores, elemento)
+    except ValueError:
+        print("Entrada inválida. Digite um número inteiro.")
+   
+    r = str(input('Quer continuar? [S/N] ')).upper()
+
+if  5 not in valores:
+      print("O valor 5 nao foi adicionado")  
+else:
+        print("O valor 5 foi adicionado")    
+
+
+print(f'Essa lista tem {len(valores)} elementos')
+valores.sort(reverse=True)
+print(valores)
