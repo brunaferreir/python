cff6afb090d7cdd7be42dc698b38a987a2ee7df8
Bruna <bruna.fsilva@aluno.faculdadeimpacta.com.br>
2025-02-18 18:53:39 -0300

    exercicios

---
+++ exercicios/ex02/00.py
@@ -0,0 +1,25 @@
+#1
+preco_uni = 12.0
+qtd = 60
+total = preco_uni * qtd
+desc = total * 0.10
+total_com_desc = total - desc
+print(total_com_desc)
+
+#2
+n = 123
+n =  n % 10 #mostrar o ultimo digito
+print(n)
+
+#3
+num = 7654
+num = num // 1000 #mostrar o primeiro digito
+print(num)
+
+#4
+salario = 1000
+idade = 23
+aumento = salario * float(idade/100)
+salarioComAumento = salario + aumento
+print(salarioComAumento)
+
